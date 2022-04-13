3d38a16179ab247ea3429f11c0b2932ac17420a4
tanvirkse101 <tanvirkse@gmail.com>
2022-04-13 19:55:52 +0600

    spring boot project initialized

---
+++ src/main/java/com/mysoft/springbootprac/SpringBootPracApplication.java
@@ -0,0 +1,13 @@
+package com.mysoft.springbootprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracApplication.class, args);
+	}
+
+}
